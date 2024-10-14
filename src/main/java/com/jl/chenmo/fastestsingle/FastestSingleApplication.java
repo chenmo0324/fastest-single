dd71ef2bcb03fbb3101f82d3e94f88d1cf965ec3
chenmo <572996940@qq.com>
2024-10-14 14:27:57 +0800

    init

---
+++ src/main/java/com/jl/chenmo/fastestsingle/FastestSingleApplication.java
@@ -0,0 +1,13 @@
+package com.jl.chenmo.fastestsingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastestSingleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastestSingleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jl/chenmo/fastestsingle/FastestSingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jl.chenmo.fastestsingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastestSingleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
